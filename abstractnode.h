50cc9ead9b9f4511077fcb7e330889a6b1c0e572
Tomasz Figa <tomasz.figa@gmail.com>
2011-05-31 15:17:26 +0200

    Inlined whatever possible in all node classes.

--- abstractnode.h
+++ abstractnode.h
@@ -8,16 +8,34 @@ class AbstractNode
 public:
     AbstractNode();
 
-    void setParent(AbstractNode *);
-    AbstractNode *getParent() const;
+	inline void setParent(AbstractNode *parent)
+	{
+		this->parent_ = parent;
+	}
+	inline AbstractNode *getParent() const
+	{
+		return parent_;
+	}
 
-    void setName(const QString &);
-    QString getName() const;
+	inline void setName(const QString &name)
+	{
+		name_ = name;
+	}
+	inline QString getName() const
+	{
+		return name_;
+	}
 
-    void setSize(qint64);
-    qint64 getSize() const;
+	inline void setSize(qint64 size)
+	{
+		size_ = size;
+	}
+	inline qint64 getSize() const
+	{
+		return size_;
+	}
 
-	virtual bool isDir() = 0;
+	virtual bool isDir() const = 0;
 
 private:
     AbstractNode *parent_;
